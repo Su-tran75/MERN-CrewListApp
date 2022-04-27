57ffaa0568b4e821b5abb7e941784267ad7e8ee8
su-tran75 <su-tran@hotmail.fr>
2022-04-28 00:06:50 +0800

    create put, delete and getAll routes and test with postman/rest client

--- server/routes/crewMember.js
+++ server/routes/crewMember.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { route } = require('express/lib/router');
 // Import crewMember model
 const crewMemberModel = require('../models/crewMember')
 
@@ -11,6 +12,38 @@ router.post('/api/crewMember', async (req, res) => {
     // Save this crew member in the db
     const saveCrewMember = await newCrewMember.save()
     res.status(200).json('Crew Member Added Successfully.');
+  } catch (err) {
+    res.json(err)
+  }
+})
+
+// create second route -- get data from db
+router.get('/api/crewMembers', async (req, res) => {
+  try {
+    const allCrewMembers = await crewMemberModel.find({});
+    res.status(200).json(allCrewMembers)
+  } catch (err) {
+    res.json(err)
+  }
+})
+
+// update crew member
+router.put('/api/crewMember/:id', async (req, res) => {
+  try {
+    // find crew member by its id and update it
+    const updateCrewMember = await crewMemberModel.findByIdAndUpdate(req.params.id, { $set: req.body });
+    res.status(200).json('CrewMember Updated')
+  } catch (err) {
+    res.json(err)
+  }
+})
+
+// delete crew member from db
+router.delete('/api/crewMember/:id', async (req, res) => {
+  try {
+    // find crew member by its id and delete it
+    const deleteCrewMember = await crewMemberModel.findByIdAndDelete(req.params.id);
+    res.status(200).json('CrewMember Deleted');
   } catch (err) {
     res.json(err)
   }
